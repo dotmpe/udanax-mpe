25c3c30a1deb605d8fd89296699498a91ed9a91f
B. van Berkum <dev@dotmpe.com>
2012-09-16 14:09:45 +0200

    Experimenting with backend commands.

--- bin/x-interactive-session.py
+++ bin/x-interactive-session.py
@@ -2,7 +2,9 @@
 """
 """
 import os
+import time
 import sys
+
 from xu88.x88 import *
 from xu88.udxutil import DOC_CONTENTS, DOC_LINKS, ENTIRE_DOC
 
@@ -83,77 +85,188 @@ if __name__ == "__main__":
 
     def example_session1():
         global xs, stream
-
+        print
         doc_addr = Address('1.1.0.1.0.1')
         xs.open_document(doc_addr, READ_ONLY, CONFLICT_FAIL)
         stream.flush()
         print "Opened document", doc_addr
+        print
         docspecset = SpecSet(VSpec(doc_addr, [ENTIRE_DOC]))
         content = xs.retrieve_contents(docspecset)
         stream.flush()
-        print "Retrieved content", content
+        print "Retrieved content"
+        print "".join(content)[0:20] + '...'
         print
+        endsets = xs.retrieve_endsets(docspecset)
+        stream.flush()
+        print "Retrieved link endsets"
+        print endsets
+        print 'Closing'
+        xs.close_document(doc_addr)
+        stream.flush()
+        return docspecset, content, endsets
 
-    def example_session2():
+    def example2():
         global xs, stream
-
         doc_addr = Address('1.1.0.1.0.1')
         xs.open_document(doc_addr, READ_ONLY, CONFLICT_FAIL)
-
-        docspec2 = xs.retrieve_vspanset(doc_addr)
-        stream.flush()
-        print "Retrieved vspanset", docspec2
-        print
-        content = xs.retrieve_contents(SpecSet(docspec2))
-        stream.flush()
-        print "Retrieved content ", content
-        print
+        #docspec2 = xs.retrieve_vspanset(doc_addr)
+        #stream.flush()
+        #print "Retrieved vspanset", docspec2
+        #print
         docvspan = xs.retrieve_vspan(doc_addr)
         stream.flush()
         print "Retrieved vspan", docvspan
         print
+        spec = SpecSet(docvspan)
+        fromlinks = xs.find_links(spec)
+        tolinks = xs.find_links(NOSPECS, spec)
+        threelinks = xs.find_links(NOSPECS, NOSPECS, spec)
+        xs.find_links(NOSPECS, NOSPECS, NOSPECS, [doc_addr, 0])
+        #homeset = xs.find_links(NOSPECS, NOSPECS, NOSPECS, [doc_addr])
+        stream.flush()
+        print
+        #print doc_addr, '<----- fromlinks', fromlinks
+        #print doc_addr, 'tolinks ------->', tolinks
+        #print doc_addr, '-- threelinks --', threelinks
+        #print doc_addr, 'homeset', homeset
+        print
+        for links in fromlinks, tolinks, threelinks:
+            for linkid in links:
+                froms = xs.follow_link(linkid, 1)
+                tos = xs.follow_link(linkid, 2)
+                threes = xs.follow_link(linkid, 3)
+                stream.flush()
+                print "Link",linkid
+                print "From",froms
+                print "To", tos
+                print "Three",threes
+                #time.sleep(1)
+        print
+        sharedspec = xs.find_documents(spec);
+        stream.flush()
+        print
+        print doc_addr, 'Shared', sharedspec
+        print 'Closing'
+        xs.close_document(doc_addr)
+        stream.flush()
+
+    def experiment():
+        print "Finding all links attached to a document or a range of documents"
+        print "is easy, but it is not possibly to find links for a given homedocument"
+        xs.open_document(Address("1.1.0.1.0.1"), READ_ONLY, CONFLICT_FAIL)
+        xs.open_document(Address("1.1.0.1.0.2"), READ_ONLY, CONFLICT_FAIL)
+        # address entire  space in docuverse 1 
+        vspec = VSpec(Address("1"), [
+            Span(Address("0.0"), Address("1.0"))
+        ])
+#        print xs.find_links(SpecSet(vspec), NOSPECS, NOSPECS)
+#        print xs.find_links(SpecSet(vspec), SpecSet(vspec), SpecSet(VSpec))
+        print xs.find_links(SpecSet(vspec), SpecSet(vspec), SpecSet(vspec), [
+                Address("1.1.0.1.0.1"),
+                Address("1.1.0.1.0.2")
+            ])
+        xs.close_document(Address("1.1.0.1.0.1"))
+        xs.close_document(Address("1.1.0.1.0.2"))
+
+    def experiment():
+        print "All docs for first user"
+        vspec = VSpec(Address("1.1.0.1"), [
+            Span(Address("0.0"), Address("1.0"))
+        ])
+        print xs.find_documents(SpecSet(vspec))
+        #xs.close_document(Address("1.1.0.1.0.1"))
+        stream.flush()
+        print "All docs for all users"
+        vspec = VSpec(Address("1.1"), [
+            Span(Address("0.0"), Address("1.0"))
+        ])
+        print xs.find_documents(SpecSet(vspec))
+        stream.flush()
+#        spec = VSpec(Span(Address("1.1.0.1"), Offset(0, 2)))
+#        print xs.find_documents(spec)
+#        stream.flush()
+#        spec = VSpec(Address(), Span(Address("1.1"), Offset(0, 2)))
+#        print xs.find_documents(spec)
+#        stream.flush()
 
     print ""
     start_session()
 
-    print """
+    doc = """
 ----
 
-Now for example to open a document, and get its contents both data and links::
+Now for example to open a document::
     
     >>> doc_addr = Address('1.1.0.1.0.1')
     >>> xs.open_document(doc_addr, READ_ONLY, CONFLICT_FAIL)
     Address(1L, 1L, 0L, 1L, 0L, 1L)
-    >>> docspecset = SpecSet(VSpec(doc_addr, [ENTIRE_DOC]))
-    >>> docspecset
-    <SpecSet [<VSpec in 1.1.0.1.0.1, at 0.0 for 2.0>]>
+
+Now we can determin its size, and/or get all contents and link endsets within
+its range::
+
+    >>> docspec = xs.retrieve_vspanset(doc_addr)
+    >>> docspec
+    <VSpec in 1.1.0.1.0.1, at 1.1 for 0.180>
+    >>> docspecset = SpecSet(docspec)
     >>> xs.retrieve_contents(docspecset)
     ['contents...']
+    >>> xs.retrieve_endsets(docspecset)
+    (SpecSet([VSpec(Address(...), Span(...))]))
 
-One could also request the spec-set or a vspan for the data/link space,
-and see how large it is:
+The entire range may also be created without knowledge of the exact length,
+because it is sufficient to select 0.0 to 1.0. Links do not seem to be retrieved
+directly like this, so selecting 1.0 to 2.0 is not of much use.
+::
 
-    >>> docspec2 = xs.retrieve_vspanset(doc_addr)
-    >>> docspec2
-    <VSpec in 1.1.0.1.0.1, at 1.1 for 0.180>
-    >>> xs.retrieve_contents(SpecSet(docspec2))
-    ['contents...']
+    >>> docspecset = SpecSet(VSpec(doc_addr, [ENTIRE_DOC])))
+    <SpecSet [<VSpec in 1.1.0.1.0.1, at 0.0 for 2.0>]>
+
+Some methods do the same as others but return different structures.
+The both retrieve_vspanset and retrieve_vspan return the same data.
+but the vspan seems for suited. The backend always returns a single
+span of a single document. Compare to the docspec above:
+
     >>> docvspan = xs.retrieve_vspan(doc_addr)
     >>> docvspan
     <VSpan in 1.1.0.1.0.1 at 1.1 for 0.180>
 
-These example sessions are run by function example_session1 and
-example_session2.
-To reintialize the session use start_session().
+The retrieve endsets list either all from, to or three sets in the document.
+This gives not the link addresses. 'find_links' does that.
+And 'follow_link' may be used to return any endset (1 from; 2 to; 3
+three)::
 
-When using the '-l' flag you may want to use stream.flush() regularly.
+    >>> xs.find_links(sourcespec, targetspec, threespec, homespec)
+    >>> xs.follow_link(1, linkid)
+
+The next step is finding shared content. find_documents accepts a spec
+of content spans, and returns all documents transcluding that.
 
 For further reference, see FEBE protocol specification for Xu88.1 [1] and 
-read the source, in particulary x88.py
+read the source, in particulary x88.py. The names in the code may be 
+different than in de protocol specification, for the same underlying commands.
+
+Experimentation shows that the find-documents-containing function may
+also be used to find all of a users documents, or to find all documents of all
+users on a node. Equally the find-links-from-to-three may be used to get
+linkids *pointing* to ranges of documents (e.g. the entire first docuverse),
+but there seems to be a bug in the queries on the homedocument of links.
+
+It is not clear how:
+    - The entire length of the link vstream is retrieved, this is because there
+      is no working query for link homedocument.
+
+Sometimes the backend will return nonsensical data for nonsensical requests:
+    - getting endsets for the entire first vstream of doc 0 worksand strangely
+      returns a bunch of 0 spans and specs.
+
+All in all, it looks like the backend has no problem to perform as a single 
+threaded hypertext editor storage.
 
 [1] http://www.udanax.com/green/febe/protocol.html
 
 ----
 """
+    print doc
 
 # vim:set et:
